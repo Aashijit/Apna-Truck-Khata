3818ed1569c4b9d468117e950141c05bf5345f0a
Aashijit <aashijitM@gmail.com>
2022-03-26 19:33:26 +0530

    Updated

--- platforms/android/app/build/generated/source/buildConfig/debug/com/saarthi/apnatruckkhata/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/saarthi/apnatruckkhata/BuildConfig.java
@@ -7,6 +7,7 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.saarthi.apnatruckkhata";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 25;
-  public static final String VERSION_NAME = "0.0.25";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "0.0.30";
 }
